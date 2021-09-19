3f54754122b0df514c9519e4068ef12f850090f0
J Tinsley <maceojamestinsley@gmail.com>
2021-09-18 21:46:18 -0700

    Fix date/sign generation

--- script.js
+++ script.js
@@ -52,16 +52,18 @@ const determineDate = (day, leap) => {
 
 const generateDateAndTime = () => {
 
-	let day = Math.floor(Math.random() * 1461);
+	//let day = Math.floor(Math.random() * 1461);
+	let day = 79;
 	let date = [];
 	let leap = false;
+	let sign = '';
 
 	let year = ((Math.floor(Math.random() * 50)) * 4) + 1900;
-	if (day <= 366) {
+	if (day < 366) {
 		leap = true;
-	} else if (day <= 731) {
+	} else if (day < 731) {
 		year += 1;
-	} else if (day <= 1096) {
+	} else if (day < 1096) {
 		year += 2;
 	} else {
 		year += 3;
@@ -70,17 +72,58 @@ const generateDateAndTime = () => {
 	date = determineDate((day % 365 - 1), leap);
 
 	let minute = (Math.floor(Math.random() * 1440));
-	console.log(day + ' ' + minute);
-	day += (Math.round(minute / 1.44)) / 1000;
-	console.log(day);
+	day += ((Math.round(minute / 1.44)) / 1000) - 1;
 	let time = [Math.floor(minute / 60), (minute % 60)]
 	
-	
+	let adjustedDate = ((Math.floor((year - 2000) / 4)) * 1461) + day;
+
 	return {
-		year, date, time, day
+		year, date, time, day, adjustedDate, sign
 	};
 }
 
-let test1 = generateDateAndTime();
 
+const determineSunSign = (obj) => {
+	let degree = Math.abs(((obj.adjustedDate % 365.06825) / 365.06825) * 360);
+	console.log(degree)
+
+	
+	/*if (obj.day < 367) {
+		degree = (obj.day / (1460.273 / 4)) * 360
+	} else {
+		degree = (((obj.day % 365) - 1) / (1460.273 / 4)) * 360;
+	}*/
+	
+	
+	if (degree < 30) {
+		return 'Aries';
+	} else if (degree < 60) {
+		return 'Taurus';
+	} else if (degree < 90) {
+		return 'Gemini';
+	} else if (degree < 120) {
+		return 'Cancer';
+	} else if (degree < 150) {
+		return 'Leo';
+	} else if (degree < 180) {
+		return 'Virgo';
+	} else if (degree < 210) {
+		return 'Libra';
+	} else if (degree < 240) {
+		return 'Scorpio';
+	} else if (degree < 270) {
+		return 'Sagittarius';
+	} else if (degree < 300) {
+		return 'Capricorn';
+	} else if (degree < 330) {
+		return 'Aquarius';
+	} else if (degree < 360) {
+		return 'Pisces';
+	} else {
+		return 'Aries';
+	}
+}
+
+let test1 = generateDateAndTime();
+test1.sign = determineSunSign(test1);
 console.log(test1);
